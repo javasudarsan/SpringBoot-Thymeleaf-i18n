efcfe920e1fb8b5a35fb42324d7bfa3a0224153c
DIP-PC <DIP-PC@Sujit>
2020-09-30 09:36:49 +0530

    multi language withspringboot-thyleafe i18n

---
+++ src/main/java/com/Thymeleaf/i18n/SpringBootThymeleafI18nApplication.java
@@ -0,0 +1,13 @@
+package com.Thymeleaf.i18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafI18nApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafI18nApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Thymeleaf/i18n/SpringBootThymeleafI18nApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Thymeleaf.i18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafI18nApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
